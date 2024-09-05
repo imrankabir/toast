82136cbce310b2915866649b7cd998a39021152c
Imran Kabir <imran.kabir.k@gmail.com>
2024-09-05 14:14:59 +0500

    toast auto commit on 2024-09-05 02:14:59 PM

--- src/toast.js
+++ src/toast.js
@@ -1,10 +1,8 @@
-const showToast = (message, className) => {
+const show = (message, className) => {
   const toastContainer = document.querySelector("#toastContainer");
   const toast = document.createElement("div");
   toast.className = `toast ${className}`;
   toast.textContent = message || `This is a toast message.`;
   toastContainer.appendChild(toast);
-  setTimeout(() => {
-    toast.remove();
-  }, 3000);
+  setTimeout(e => toast.remove(), 3000);
 };
